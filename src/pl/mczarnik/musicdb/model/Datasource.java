2e1bd78bd1f58aff9da62ab63d3164c7f6715285
Mateusz Czarnik <mamarok@gmail.com>
2018-04-14 22:01:21 +0200

    Artists query

--- src/pl/mczarnik/musicdb/model/Datasource.java
+++ src/pl/mczarnik/musicdb/model/Datasource.java
@@ -1,8 +1,8 @@
 package pl.mczarnik.musicdb.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Datasource {
     public static final String DB_NAME = "music.db";
@@ -46,6 +46,27 @@ public class Datasource {
             }
         } catch (SQLException e) {
             System.out.println("Couldn't close connection: " + e.getMessage());
+        }
+    }
+
+    public List<Artist> queryArtists() {
+        // Using try with resources to ensure that close method will be called for both Statement and ResultSet
+        try (Statement statement = conn.createStatement();
+             ResultSet results = statement.executeQuery("SELECT * FROM " + TABLE_ARTISTS)) {
+
+            List<Artist> artists = new ArrayList<>();
+            while (results.next()) {
+                Artist artist = new Artist();
+                artist.setId(results.getInt(COLUMN_ARTIST_ID));
+                artist.setName(results.getString(COLUMN_ARTIST_NAME));
+                artists.add(artist);
+            }
+
+            return artists;
+
+        } catch (SQLException e) {
+            System.out.println("Query failed: " + e.getMessage());
+            return null;
         }
     }
 }
