ff8f029a702adbb08c785c962f75507523870b4b
Mateusz Czarnik <mamarok@gmail.com>
2018-04-14 23:20:36 +0200

    Test commit

--- src/pl/mczarnik/musicdb/Main.java
+++ src/pl/mczarnik/musicdb/Main.java
@@ -24,6 +24,7 @@ public class Main {
             System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
         }
 
+        // Close datasource
         datasource.close();
     }
 }
