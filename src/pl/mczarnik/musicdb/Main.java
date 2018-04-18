4ad0e7ae8f3c0eeb1a2657bc4855a6eeed056dca
Mateusz Czarnik <mamarok@gmail.com>
2018-04-18 07:24:48 +0200

    Get ResultSet metadata

--- src/pl/mczarnik/musicdb/Main.java
+++ src/pl/mczarnik/musicdb/Main.java
@@ -2,6 +2,7 @@ package pl.mczarnik.musicdb;
 
 import pl.mczarnik.musicdb.model.Artist;
 import pl.mczarnik.musicdb.model.Datasource;
+import pl.mczarnik.musicdb.model.SongArtist;
 
 import java.util.List;
 
@@ -24,11 +25,25 @@ public class Main {
             System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
         }
 
-        List<String> albumsForArtist = datasource.queryAlbumsForArtist("Iron Maiden", Datasource.ORDER_BY_ASC);
+        List<String> albumsForArtist = datasource.queryAlbumsForArtist("Carole King", Datasource.ORDER_BY_ASC);
 
-        for(String album : albumsForArtist){
+        for (String album : albumsForArtist) {
             System.out.println(album);
         }
+
+        List<SongArtist> songArtists = datasource.queryArtistsForSong("Heartless", Datasource.ORDER_BY_ASC);
+        if (songArtists == null) {
+            System.out.println("Couldn't find the artist for the song");
+        }
+
+        for (SongArtist artist : songArtists) {
+            System.out.println("Artist name = " + artist.getArtistName() +
+                    " Album name = " + artist.getAlbumName() +
+                    " Track = " + artist.getTrack());
+        }
+
+
+        datasource.querySongsMetadata();
 
         // Close datasource
         datasource.close();
